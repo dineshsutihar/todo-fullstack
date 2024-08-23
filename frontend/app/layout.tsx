e974256203da0784f884124435d7e85d1d12d074
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-08-24 04:06:42 +0530

    frontend: initial Commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Todo Fullstack",
+  description: "Create, read, update, and delete todos.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Todo Frontend</h1>
+    </div>
+  );
+}
